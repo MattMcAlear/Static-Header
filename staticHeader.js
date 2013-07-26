2952c8e21f4c66799c271f2144fa7988bca316a9
Matt McAlear <mcalear.matt@gmail.com>
2013-07-26 16:07:14 -0400

    - Added real file

--- staticHeader.js
+++ staticHeader.js
@@ -1,391 +1,63 @@
-// Purpose: Animated Logo for Design Firm
-// By: 		Matt M.
-// Date:	08-26-2012
-setTimeout(function(){
-	increment=1;
-},950);
-var canvas2 = document.getElementById('logo');
-var ctx2 = canvas2.getContext('2d');
+// Purpose:	For serving static headers to table headers
+// By: 		Matt McAlear
+// Date:	7/26/13
 
-var x=canvas2.width/2;
-var y=canvas2.height/2;
-
-var rectWidth = 50;
-var rectHeight = 20;
-
-var rectWidth2 = 70;
-var rectHeight2 = 20;
-
-var tSide=50;
-var tHeight = tSide * (Math.sqrt(3)/2);
-
-var tSideSmall=tSide/2;
-var tHeightSmall = tSideSmall * (Math.sqrt(3)/2);
-
-var rotate = 1;
-var rotate2 = 1;
-
-var increment = 10
-
-var t1 = "#633";
-var t2 = "#069";
-var t3 = "#969";
-var t4 = "#C96";
-var t5 = "black";
-var t6 = "gray";
-
-function init(){
-	return setInterval(draw, 50);
+function makeStaticHeader(elementID){
+	this._element 	= document.getElementById(elementID);
+	this._jElement 	= $('#'+elementID);
+	this._doc		= $(document);
+	this._header	= document.getElementById('staticHead');
 }
 
-function clear(){
-	ctx2.clearRect(0,0,canvas2.width,canvas2.height);	
-}
-
-function draw(){
-	clear();
+makeStaticHeader.prototype = {
+	//restore constructor
+	constructor: makeStaticHeader,
 	
-	triangle();
-}
-
-function triangle(){
-	if(rotate > 360){
-		rotate=1;	
-	}else{
-		rotate=rotate+increment;
+	init: function(){
+		that = this;
+		window.onscroll = function(){
+	        if(that._doc.scrollTop() >= that._jElement.position().top){
+		        that._header.style.display = 'block';
+	        }else{
+		        that._header.style.display = 'none';
+	        }
+	    };
+	},
+	
+	makeHeader: function(){
+		that = this;
+    	var w = this._element.offsetWidth;
+	    var body=document.getElementsByTagName('body')[0];
+		var table=document.createElement('table');
+		table.style.width=w+'px';
+		table.style.position='fixed';
+		table.style.top='0px';
+		table.style.display='none';
+		table.style.opacity=.75;
+		table.style.backgroundColor='#888';
+		table.style.color='white';
+		table.setAttribute('border','1');
+		table.setAttribute('id', 'staticHead');
+		
+		var tbody=document.createElement('tbody');
+		var tr = this._element.cloneNode(true);
+		tr.style.width=w+'px';
+		var total = 0;
+		for(var i=0; i<this._element.cells.length; i++){
+			var pad;
+			if(i==0 || (i+1==this._element.cells.length)){ pad=2; }else{ pad = 4; }
+			tr.cells[i].style.width=(this._element.cells[i].offsetWidth-pad)+'px';
+		}
+		
+		//Set elements
+		tbody.appendChild(tr);
+		table.appendChild(tbody);
+		body.appendChild(table);
+		
+		//Set new static header
+		that._header = document.getElementById('staticHead');
+		
+		//init listener
+		that.init();
 	}
-	
-	if(rotate2 > 360){
-		rotate2=1;
-	}else{
-		rotate2=rotate2+5;	
-	}
-	ctx2.save();
-	ctx2.beginPath();
-	ctx2.moveTo(x,y);
-	ctx2.lineTo(x+(tSide/2),y+(tHeight));
-	ctx2.lineTo(x+(tSide/2)-(tSide),y+(tHeight));
-	ctx2.lineTo(x,y);
-	ctx2.fillStyle="#333"
-	ctx2.fill();
-	ctx2.closePath();
-	ctx2.clip();
-	/*
-	//5th triangles
-	ctx2.save();
-	// update
-	var date = new Date();
-	var time = date.getTime();
-	var amplitude = tHeightSmall;
-	// in ms
-	var period = 5000;
-	var centerY = canvas2.height / 2 + tHeight / 2;
-	var nextY = amplitude * Math.sin(time * 2 * Math.PI / period) + centerY;
-	ctx2.translate(canvas2.width / 2, nextY);
-	ctx2.rotate((Math.PI/180)*rotate2);
-	ctx2.beginPath();
-	ctx2.fillStyle="black";
-	
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2)-(tSideSmall),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.beginPath();
-	ctx2.fillStyle="black";
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tHeightSmall),0-(tSideSmall/2));
-	ctx2.lineTo(0+(tHeightSmall),0-(tSideSmall/2)+(tSideSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.beginPath();
-	ctx2.fillStyle="black";
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tHeightSmall),0+(tSideSmall/2));
-	ctx2.lineTo(0-(tHeightSmall),0+(tSideSmall/2)-(tSideSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.beginPath();
-	ctx2.fillStyle="black";
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0-(tSideSmall/2)+(tSideSmall),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	ctx2.restore();
-	*/
-	//4th triangles
-	ctx2.save();
-	ctx2.beginPath();
-	ctx2.moveTo(x,y+(tHeightSmall*2));
-	ctx2.lineTo(x+(tSideSmall/2),y+(tHeightSmall*2)-(tHeightSmall));
-	ctx2.lineTo(x+(tSideSmall/2)-(tSideSmall),y+(tHeightSmall*2)-(tHeightSmall));
-	ctx2.lineTo(x,y+(tHeightSmall*2));
-	ctx2.closePath();
-	ctx2.clip();
-	
-	ctx2.save();
-	ctx2.translate(canvas2.width / 2, canvas2.height / 2+(tHeightSmall*2));
-	ctx2.rotate((Math.PI/180)*rotate);
-	//1
-	ctx2.beginPath();
-	ctx2.fillStyle=t1;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2)-(tSideSmall),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//2
-	ctx2.beginPath();
-	ctx2.fillStyle=t2;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tHeightSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//3
-	ctx2.beginPath();
-	ctx2.fillStyle=t3;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0+(tHeightSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//4
-	ctx2.beginPath();
-	ctx2.fillStyle=t4;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//5
-	ctx2.beginPath();
-	ctx2.fillStyle=t5;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//6
-	ctx2.beginPath();
-	ctx2.fillStyle=t6;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.restore();
-	ctx2.restore();
-	
-	//1st triangles
-	ctx2.save();
-	ctx2.beginPath();
-	ctx2.moveTo(x,y);
-	ctx2.lineTo(x+(tSideSmall/2),y+(tHeightSmall));
-	ctx2.lineTo(x+(tSideSmall/2)-(tSideSmall),y+(tHeightSmall));
-	ctx2.lineTo(x,y);
-	ctx2.closePath();
-	ctx2.clip();
-	
-	ctx2.save();
-	ctx2.translate(canvas2.width / 2, canvas2.height / 2);
-	ctx2.rotate((Math.PI/180)*rotate);
-	//1
-	ctx2.beginPath();
-	ctx2.fillStyle=t1;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2)-(tSideSmall),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//2
-	ctx2.beginPath();
-	ctx2.fillStyle=t2;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tHeightSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//3
-	ctx2.beginPath();
-	ctx2.fillStyle=t3;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0+(tHeightSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//4
-	ctx2.beginPath();
-	ctx2.fillStyle=t4;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//5
-	ctx2.beginPath();
-	ctx2.fillStyle=t5;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//6
-	ctx2.beginPath();
-	ctx2.fillStyle=t6;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.restore();
-	ctx2.restore();
-	
-	//2nd triangles
-	ctx2.save();
-	ctx2.beginPath();
-	ctx2.moveTo(x-tSideSmall,y+(tHeightSmall*2));
-	ctx2.lineTo(x-tSideSmall+(tSideSmall/2),y+(tHeightSmall*2)-(tHeightSmall));
-	ctx2.lineTo(x,y+(tHeightSmall*2));
-	ctx2.lineTo(x-tSideSmall,y+(tHeightSmall*2));
-	ctx2.closePath();
-	ctx2.clip();
-	
-	ctx2.save();
-	ctx2.translate(canvas2.width / 2-tSideSmall, canvas2.height / 2+(tHeightSmall*2));
-	ctx2.rotate((Math.PI/180)*rotate+67);
-	//1
-	ctx2.beginPath();
-	ctx2.fillStyle=t1;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2)-(tSideSmall),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//2
-	ctx2.beginPath();
-	ctx2.fillStyle=t2;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tHeightSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//3
-	ctx2.beginPath();
-	ctx2.fillStyle=t3;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0+(tHeightSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//4
-	ctx2.beginPath();
-	ctx2.fillStyle=t4;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//5
-	ctx2.beginPath();
-	ctx2.fillStyle=t5;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//6
-	ctx2.beginPath();
-	ctx2.fillStyle=t6;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.restore();
-	ctx2.restore();
-	
-	//3rd triangles
-	ctx2.save();
-	ctx2.beginPath();
-	ctx2.moveTo(x+tSideSmall,y+(tHeightSmall*2));
-	ctx2.lineTo(x+tSideSmall-(tSideSmall/2),y+(tHeightSmall*2)-(tHeightSmall));
-	ctx2.lineTo(x,y+(tHeightSmall*2));
-	ctx2.lineTo(x+tSideSmall,y+(tHeightSmall*2));
-	ctx2.closePath();
-	ctx2.clip();
-	
-	ctx2.save();
-	ctx2.translate(canvas2.width / 2+tSideSmall, canvas2.height / 2+(tHeightSmall*2));
-	ctx2.rotate((Math.PI/180)*rotate-67);
-	//1
-	ctx2.beginPath();
-	ctx2.fillStyle=t1;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2)-(tSideSmall),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//2
-	ctx2.beginPath();
-	ctx2.fillStyle=t2;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tHeightSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//3
-	ctx2.beginPath();
-	ctx2.fillStyle=t3;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0+(tSideSmall),0);
-	ctx2.lineTo(0+(tHeightSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//4
-	ctx2.beginPath();
-	ctx2.fillStyle=t4;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0+(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//5
-	ctx2.beginPath();
-	ctx2.fillStyle=t5;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0-(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	//6
-	ctx2.beginPath();
-	ctx2.fillStyle=t6;
-	ctx2.moveTo(0,0);
-	ctx2.lineTo(0-(tSideSmall),0);
-	ctx2.lineTo(0-(tSideSmall/2),0+(tHeightSmall));
-	ctx2.lineTo(0,0);
-	ctx2.fill();
-	
-	ctx2.restore();
-	ctx2.restore();
-	
-	//Draw outline
-	ctx2.beginPath();
-	ctx2.moveTo(x,y);
-	ctx2.lineTo(x+(tSide/2),y+(tHeight));
-	ctx2.lineTo(x+(tSide/2)-(tSide),y+(tHeight));
-	ctx2.lineTo(x,y);
-	ctx2.closePath();
-	ctx2.lineWidth=3;
-	ctx2.stroke();		
 }
-
-init();
